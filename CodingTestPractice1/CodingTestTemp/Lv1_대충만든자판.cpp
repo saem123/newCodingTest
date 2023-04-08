7fbc097c12de4d49db06041de65dec2ac850df38
saem123 <ahnsem123@gmail.com>
2023-04-08 18:10:07 +0900

    달리기 경주 문제 커밋

---
+++ CodingTestPractice1/CodingTestTemp/Lv1_대충만든자판.cpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "programmers.h"
+#include <vector>
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+
+using namespace std;
+void Programmers::Lv1_대충만든자판()
+{
+
+}
+
+vector<int> solution(vector<string> keymap, vector<string> targets) {
+	vector<int> answer;
+	
+	return answer;
+}
--- CodingTestPractice1/CodingTestTemp/programmers.h
+++ CodingTestPractice1/CodingTestTemp/programmers.h
@@ -10,7 +10,8 @@ public:
 	void Lv1_공원산책();
 	void Lv1_바탕화면정리();
 	void Lv1_덧칠하기();
-
+	void Lv1_달리기경주();
+	void Lv1_대충만든자판();
 };
 
 
