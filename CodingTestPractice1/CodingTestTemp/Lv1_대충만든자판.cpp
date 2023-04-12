cd8d7497730e5163d12975f9f64ad1cad515af14
saem123 <ahnsem123@gmail.com>
2023-04-12 14:52:46 +0900

    대충만든자판 커밋

--- CodingTestPractice1/CodingTestTemp/Lv1_대충만든자판.cpp
+++ CodingTestPractice1/CodingTestTemp/Lv1_대충만든자판.cpp
@@ -14,22 +14,35 @@ void Programmers::Lv1_대충만든자판()
 
 vector<int> solution(vector<string> keymap, vector<string> targets) {
 	vector<int> answer;
-
-	map<string, int> dic;
-
-
-	for (int index = 0; index < targets.size(); index++)
+	for (int i = 0; i < targets.size(); i++)
 	{
-		dic.insert(make_pair(targets[index], index));
-	}
-
-	
-	for (int i = 0; i < keymap.size(); i++)
-	{
-		for (int j = 0; j < keymap[i].size(); j++)
+		answer.push_back(0);
+		for (int j = 0; j < targets[i].size(); j++)
 		{
-			//모르겠다.. 더고민
-			break;
+			char c = targets[i][j];
+			int type = 101, flag = 1;
+			
+			for (int k = 0; k < keymap.size(); k++)
+			{
+				for (int l = 0; l < keymap[k].size(); l++)
+				{
+					if (keymap[k][l] == c)
+					{
+						if (type > l + 1)
+						{
+							type = l + 1;
+						}
+						flag = 0;
+						break;
+					}
+				}
+			}
+			if (flag)
+			{
+				answer[i] = -1;
+				break;
+			}
+			answer[i] += type;
 		}
 	}
 	return answer;
